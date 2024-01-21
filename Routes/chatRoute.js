30d63ddd6d906936998d29eb746433fa157a6408
hrixtik <Hritik.Kumar@cogoport.com>
2024-01-21 21:28:29 +0530

    chat app backend

---
+++ Routes/chatRoute.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const {
+  findUserChats,
+  createChat,
+  findChat,
+} = require("../Controllers/chatController");
+
+const router = express.Router();
+
+router.post("/", createChat);
+router.get("/:userId", findUserChats);
+router.get("/find/:firstId/:secondId", findChat);
+
+module.exports = router;
